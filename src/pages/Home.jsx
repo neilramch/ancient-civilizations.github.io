fff178a55d5781503ec3f45d1ff3dca825c680e3
Neil Ramchandani <neilramch@Neils-MacBook-Air.local>
2025-02-14 22:34:47 -0500

    lost the draggable tags were a drag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,64 +1,35 @@
-import React from 'react';
-import Draggable from 'react-draggable';
+import React, { useEffect, useRef } from 'react';
 import './Home.css';
 import DailyFact from '../components/DailyFact';
 import News from '../components/News';
 
 const Home = () => {
 
-  
   return (
     <div>
+
       <div className='home-container'>
-        <div className='widgets-container'>
-
-          {/* Daily Fact Widget - Draggable */}
-          <Draggable bounds=".widgets-container" axis="both"  defaultPosition={{ x: -400, y: 50 }}>
-            <div className='fact-container draggable-widget'>
-              <DailyFact />
-            </div>
-          </Draggable>
-
-          {/* News Widget - Draggable */}
-          <Draggable bounds=".widgets-container"  defaultPosition={{ x: 400, y: 50 }}  cancel="a"  >
-            <div className='news-container draggable-widget'>
-              <News />
-            </div>
-          </Draggable>
-
+      <div className='widgets-container'>
+        <div className='fact-container'>
+          <DailyFact />
         </div>
-
-        {/* Interactive Map */}
+        <div className='news-container'>
+          <News />
+        </div>
+        </div>
         <div className='map-container'>
           <h1>Interactive Map</h1>
           <h3>(See how the globe looked throughout the ages)</h3>
-          <iframe 
-            className='interactive-map' 
-            src="https://www.runningreality.org/?lesson=none&key=FREEACCESS" 
-            referrerPolicy="origin"
-          >
-            Running Reality
-          </iframe>
-          <p>via <a href='https://www.runningreality.org/' target="_blank" rel="noopener noreferrer">
-            The Running Reality Organization
-          </a></p>
+          <iframe className='interactive-map' src="https://www.runningreality.org/?lesson=none&key=FREEACCESS" referrerolicy="origin">Running Reality</iframe>
+          <p>via <a href='https://www.runningreality.org/' target="_blank">The Running Reality Organization</a></p>
         </div>
-
-        {/* Timeline Embed */}
-        <iframe 
-          src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1-0jPkFmmwgZ8C7vc6jIB2F-ICVWY9LfjKsDQe14GYR0&font=Default&lang=en&initial_zoom=2&height=650' 
-          width='100%' 
-          height='650' 
-          allowFullScreen 
-          frameBorder='0'
-        ></iframe>
-
-        {/* Timeline Images */}
+        <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1-0jPkFmmwgZ8C7vc6jIB2F-ICVWY9LfjKsDQe14GYR0&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen = "true" mozallowfullscreen = "true" allowFullScreen frameBorder='0'></iframe>
         <div className='timelines'>
-          <img src='/ancient-timeline.svg' alt="Ancient Timeline" />
-          <img src='/middle-timeline.svg' alt="Middle Timeline" />
-          <img src='/modern-timeline.svg' alt="Modern Timeline" />
+          <img src='/ancient-timeline.svg'></img>
+          <img src='/middle-timeline.svg'></img>
+          <img src='/modern-timeline.svg'></img>
         </div>
+
       </div>    
     </div>
   );
